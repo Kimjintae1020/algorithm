0f5e80de4ccdfa05e3bccedeb761a4d7c85b80c1
Kimjintae1020 <aasdxcz123@gmail.com>
2024-04-13 23:14:11 +0900

    240413

---
+++ py/10815.py
@@ -0,0 +1,12 @@
+input()
+n = set(map(int,input().split()))
+print(n)
+
+input()
+m = list(map(int,input().split()))
+
+for i in m:
+    if i in n:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
