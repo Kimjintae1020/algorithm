64ff148e1dd5260eacd77ca54e82772f019ac99b
Kimjintae1020 <aasdxcz123@gmail.com>
2024-04-18 02:14:03 +0900

    240418

---
+++ py/2559.py
@@ -0,0 +1,12 @@
+date, temp = map(int,input().split())
+
+num_li = list(map(int,input().split()))
+
+
+max = num_li[0]
+
+for i in range(temp):
+    sum = num_li[i]
+
+for i in range(temp-1,date): #2~10
+
